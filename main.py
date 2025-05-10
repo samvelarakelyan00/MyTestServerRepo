89bc3cf02cdf2265efdc7e3be17e3bf8553a444f
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-05-10 14:52:06 +0400

    add new API to get all users

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+all_users = [
+    {"name": "Samvel", "email": "samvel.arakelyan00@gmail.com"},
+    {"name": "Tatev", "email": "tatevikyeghiazaryann5@gmail.com"},
+]
 
 @app.get("/")
 def main():
     return {"message": "OK"}
+
+
+@app.get("/users")
+def get_all_users():
+    return all_users
